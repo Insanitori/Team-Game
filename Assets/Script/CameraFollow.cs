469de13bcdd2db89633746a0f391d5c410e10637
Insanitori <92389074+Insanitori@users.noreply.github.com>
2021-11-03 16:22:47 -0400

    update

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -15,6 +15,6 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = new Vector3(Player.transform.position.x, 1f, -15);
+        transform.position = new Vector3(Player.transform.position.x, 4f, -11);
     }
 }
--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject zoomiePrefab;
+    public float begin;
+    public float end;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("spawnZoomies", 10, 20.0f);
+        InvokeRepeating("spawnZoomies", 5, 20.0f);
     }
 
     // Update is called once per frame
@@ -19,7 +21,7 @@ public class SpawnManager : MonoBehaviour
 
     void spawnZoomies()
     {
-        float spawnPosX = Random.Range(8, 95);
+        float spawnPosX = Random.Range(begin, end);
         Vector3 randomPos = new Vector3(spawnPosX, 0, -3);
         Instantiate(zoomiePrefab, randomPos, zoomiePrefab.transform.rotation);
     }
