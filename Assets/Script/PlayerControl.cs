816d3d06e751c744aa9a00dd0991c13f75a6a1d9
Insanitori <92389074+Insanitori@users.noreply.github.com>
2021-11-30 11:46:08 -0500

    houi

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -69,6 +70,10 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
 
         hInport = Input.GetAxis("Vertical");
         vInport = Input.GetAxis("Horizontal");
